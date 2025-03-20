a15b03dce38bb3369ffd57ffc8ae0d27141ac455
prakash soni <prakashsoni0024@gmail.com>
2025-03-20 23:44:36 +0530

    add number guessing code

---
+++ script.js
@@ -0,0 +1,22 @@
+// print 1 to 5
+
+// for(let count=1; count<=5; count++) {
+//     console.log("Apna college");
+// }
+
+// Calculate sum of 1 to 5
+// let sum = 0;
+// for(let i=1; i<=5; i++) {
+//     sum = sum + i;
+// }
+
+// console.log("sum =", sum);
+
+
+// console.log("loops has ended");
+
+let i = 1;
+while(i <=10) {
+    i++;
+    console.log("Apna-colege");
+}
