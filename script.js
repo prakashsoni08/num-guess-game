e4a776bf1eca3007078411bdf182cc8ef0248975
prakash soni <prakashsoni0024@gmail.com>
2025-03-20 23:48:13 +0530

    update code

--- script.js
+++ script.js
@@ -1,22 +1,11 @@
-// print 1 to 5
+// Practice Qs2
 
-// for(let count=1; count<=5; count++) {
-//     console.log("Apna college");
-// }
+let gameNum = 25;
 
-// Calculate sum of 1 to 5
-// let sum = 0;
-// for(let i=1; i<=5; i++) {
-//     sum = sum + i;
-// }
+let userNum = prompt("Guess the game number :");
+while(userNum != gameNum) {
+    userNum = prompt("You entered wrong number, Guess again : ");
 
-// console.log("sum =", sum);
-
-
-// console.log("loops has ended");
-
-let i = 1;
-while(i <=10) {
-    i++;
-    console.log("Apna-colege");
 }
+
+console.log("congratulations, you entered the right number");
